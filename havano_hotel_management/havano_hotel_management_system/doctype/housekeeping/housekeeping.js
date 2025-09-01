bea5ff51992d648228112c00605579232d299a6a
Concern Simbarashe Gobvu <37830258+EngConcern@users.noreply.github.com>
2025-09-01 12:57:14 +0200

    house keeeping

--- havano_hotel_management/havano_hotel_management_system/doctype/housekeeping/housekeeping.js
+++ havano_hotel_management/havano_hotel_management_system/doctype/housekeeping/housekeeping.js
@@ -6,3 +6,16 @@
 
 // 	},
 // });
+
+frappe.listview_settings['Housekeeping'] = {
+    add_fields: ["room", "cleaning_status", "last_cleaned", "assigned_staff"],
+    get_indicator: function (doc) {
+        if (doc.cleaning_status === "Clean") {
+            return [__("Clean"), "green", "cleaning_status,=,Clean"];
+        } else if (doc.cleaning_status === "Dirty") {
+            return [__("Dirty"), "red", "cleaning_status,=,Dirty"];
+        } else if (doc.cleaning_status === "Out of Order") {
+            return [__("Out of Order"), "orange", "cleaning_status,=,Out of Order"];
+        }
+    }
+};
